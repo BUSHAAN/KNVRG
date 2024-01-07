8fcf39eaacb373603ea4f1f69106853a8a3a5045
Bushaan Gunatilake <bushaangunatilake@gmail.com>
2024-01-07 17:16:29 +0530

    Create the LeftBar

--- src/components/shared/LeftBar.tsx
+++ src/components/shared/LeftBar.tsx
@@ -1,8 +1,78 @@
+import { sidebarLinks } from "@/constants";
+import { useUserContext } from "@/context/useUserContext";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { INavLink } from "@/types";
+import { useEffect } from "react";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import Logout from "../../assets/icons/logout.svg";
+import ProfilePlaceholder from "../../assets/icons/profile-placeholder.svg";
+import Logo from "../../assets/images/logo.jpg";
+import { Button } from "../ui/button";
 
 const LeftBar = () => {
-  return (
-    <div>LeftBar</div>
-  )
-}
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { user } = useUserContext();
 
-export default LeftBar
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+    }
+  }, [isSuccess, navigate]);
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <img src={Logo} alt="Logo" width={170} height={36} />
+        </Link>
+        <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+          <img
+            src={user.imageUrl || ProfilePlaceholder}
+            className="h-14 w-14 rounded-full"
+            alt=""
+          />
+          <div className="flex flex-col">
+            <p className="body-bold">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname == link.route;
+            return (
+              <li
+                key={link.label}
+                className={`leftsidebar-link group ${isActive && "bg-primary-500"}`}
+              >
+                <NavLink
+                  to={link.route}
+                  className="flex gap-4 items-center p-4"
+                >
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    className={`group-hover:invert-white ${
+                      isActive && "invert-white"
+                    }`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}
+          >
+            <img src={Logout} alt="Logout" />
+            <p className="small-medium lg:base-medium">Logout</p>
+          </Button>
+    </nav>
+  );
+};
+
+export default LeftBar;
---
+++ src/constants/index.ts
@@ -0,0 +1,51 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/src/assets/icons/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/src/assets/icons/wallpaper.svg",
+      route: "/explore",
+      label: "Explore",
+    },
+    {
+      imgURL: "/src/assets/icons/people.svg",
+      route: "/all-users",
+      label: "People",
+    },
+    {
+      imgURL: "/src/assets/icons/bookmark.svg",
+      route: "/saved",
+      label: "Saved",
+    },
+    {
+      imgURL: "/src/assets/icons/gallery-add.svg",
+      route: "/create-post",
+      label: "Create Post",
+    },
+  ];
+  
+
+  export const bottombarLinks = [
+    {
+      imgURL: "",
+      route: "/src/",
+      label: "Home",
+    },
+    {
+      imgURL: "/src/assets/icons/wallpaper.svg",
+      route: "/explore",
+      label: "Explore",
+    },
+    {
+      imgURL: "/src/assets/icons/bookmark.svg",
+      route: "/saved",
+      label: "Saved",
+    },
+    {
+      imgURL: "/src/assets/icons/gallery-add.svg",
+      route: "/create-post",
+      label: "Create",
+    },
+  ];
