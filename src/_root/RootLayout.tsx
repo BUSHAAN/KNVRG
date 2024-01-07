6787d8b166812686da6301acca1363e6a447ad3d
Bushaan Gunatilake <bushaangunatilake@gmail.com>
2024-01-07 16:14:10 +0530

    Create HomePage (top-bar) and sign out function

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,19 @@
+import BottomBar from "@/components/shared/BottomBar";
+import LeftBar from "@/components/shared/LeftBar";
+import TopBar from "@/components/shared/TopBar";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
-  )
-}
+    <div className="w-full md:flex">
+      <TopBar />
+      <LeftBar />
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+      <BottomBar/>
+    </div>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
